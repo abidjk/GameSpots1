d097560730eac054a7e5eb81e3ffcee0078d313b
abidjk <abidjumailk@gmail.com>
2022-02-01 13:52:17 +0530

    first

---
+++ game/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .import views
+
+urlpatterns=[
+
+    path('',views.Home),
+    path('signup',views.Signup, name="sign"),
+]
---
+++ game/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def Home(request):
+    return render(request, 'home.html')
+
+def Signup(request):
+    return render(request, 'signup.html')
+
+
